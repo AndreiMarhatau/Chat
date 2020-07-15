a0c3b0b333af0d1bc7e31118fe21b3d2ae925ab0
Андрей Маргатов <margatov.andrey@gmail.com>
2020-07-15 17:27:11 +0300

    Fix prod config file for webpack

--- ChatServer/dev-react-app/webpack.prod.js
+++ ChatServer/dev-react-app/webpack.prod.js
@@ -3,6 +3,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const PATH = {
+  SRC: path.join(__dirname, '/src')
+};
 
 module.exports = merge(common, {
   optimization: {
